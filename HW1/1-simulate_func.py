5075496be04291945965489903a5b01e3b94b331
Jin Ma <jin7@login002.palmetto.clemson.edu>
2024-01-21 16:54:49 -0500

    HW1 1.1 simulate a function done

---
+++ HW1/1-simulate_func.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+class DNN1(nn.Module):
+    def __init__(self):
+        super(DNN1, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(1, 10),
+            nn.ReLU(),
+            nn.Linear(10, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10),
+            nn.ReLU(),
+            nn.Linear(10, 1)
+        )
+
+    def forward(self, x):
+        return self.main(x)
+
+class DNN2(nn.Module):
+    def __init__(self):
+        super(DNN2, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(1, 20),
+            nn.ReLU(),
+            nn.Linear(20, 20, bias=False),
+            nn.ReLU(),
+            nn.Linear(20, 1),
+        )
+
+    def forward(self, x):
+        return self.main(x)
+
+
+def train_model(model, train_loader, optimizer, criterion, epochs=1000):
+    model.train()
+    losses = []
+    progress_bar = tqdm(range(epochs))
+    for epoch in progress_bar:
+        epoch_loss = 0
+        for i, (x, y) in enumerate(train_loader):
+            optimizer.zero_grad()
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+        epoch_loss /= len(train_loader)
+        losses.append(epoch_loss)
+        progress_bar.set_description(f'Epoch: {epoch+1}/{epochs}. Loss: {epoch_loss:.5f}')
+    return losses
+
+if __name__ == "__main__":
+    # Create the model
+    model1 = DNN1()
+    model2 = DNN2()
+
+    # print the number of parameters in the model
+    print(f'Number of parameters: DNN1={sum(p.numel() for p in model1.parameters())}, DNN2={sum(p.numel() for p in model2.parameters())}')
+
+    # Create the data
+    x = torch.linspace(-10, 10, 10000).reshape(-1, 1)
+    y = torch.sin(x) + torch.cos(2*x)
+
+    # parameters
+    train_loader = torch.utils.data.DataLoader(list(zip(x, y)), batch_size=2000, shuffle=True)
+    lr = 1e-3
+    optimizer1 = optim.Adam(model1.parameters(), lr=lr)
+    optimizer2 = optim.Adam(model2.parameters(), lr=lr)
+    criterion = nn.MSELoss()
+    epochs = 1000
+
+    # Train the model
+    losses1 = train_model(model1, train_loader, optimizer1, criterion, epochs)
+    losses2 = train_model(model2, train_loader, optimizer2, criterion, epochs)
+
+    # Plot the loss
+    fig1 = plt.figure(figsize=(10, 5))
+    plt.plot(losses1, label='DNN1', color='cyan')
+    plt.plot(losses2, label='DNN2', color='orange')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    # plt.show()
+    fig1.savefig('func_loss.png')
+    plt.close()
+
+    # Plot the prediction
+    fig2 = plt.figure(figsize=(10, 5))
+    plt.plot(x, y, label='GT', color='blue')
+    plt.plot(x, model1(x).detach().numpy(), label='DNN1', color='cyan')
+    plt.plot(x, model2(x).detach().numpy(), label='DNN2', color='orange')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    # plt.show()
+    fig2.savefig('func_prediction.png')
+    plt.close()
+
